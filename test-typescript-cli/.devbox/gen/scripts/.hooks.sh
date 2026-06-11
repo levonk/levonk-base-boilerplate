151f100d1b14faac0315520be4eb485f2afdd8c4
Levon Karayan <micro@LZKMBP2018.lan>
2026-06-10 17:56:24 -0700

    Remove test-typescript-cli directory
    Clean up test artifact directory that was used for TypeScript CLI boilerplate validation

--- test-typescript-cli/.devbox/gen/scripts/.hooks.sh
+++
@@ -1,1 +0,0 @@
-just bootstrap-internal
--- test-typescript-cli/.devbox/gen/scripts/dev.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-if [ -z "$__DEVBOX_SKIP_INIT_HOOK_dd7543c3a631e59e6f2e4773bfaa1ff9a062bdeeefbd79093d9985c8093db80c" ]; then
-    . "/Users/micro/p/gh/levonk/levonk-base-boilerplate/test-typescript-cli/.devbox/gen/scripts/.hooks.sh"
-fi
-
-tsx watch src/index.ts
